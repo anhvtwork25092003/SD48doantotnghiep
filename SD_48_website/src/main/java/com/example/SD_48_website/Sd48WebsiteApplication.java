47e69142f14392a906f44cbd0fb3d02342553d68
phuocvv <phuocvv@ftl.vn>
2023-10-01 21:30:50 +0700

    all project

---
+++ SD_48_website/src/main/java/com/example/SD_48_website/Sd48WebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.SD_48_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd48WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd48WebsiteApplication.class, args);
+    }
+
+}
