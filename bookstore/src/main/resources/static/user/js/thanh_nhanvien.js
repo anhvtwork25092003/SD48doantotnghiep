18e74ce45fc7306266cb63a60643319b81e013c9
thanhph25817 <thanhnvph25817@fpt.edu.vn>
2023-10-11 16:35:09 +0700

    cua thanh chieu nay

--- bookstore/src/main/resources/static/user/js/thanh_nhanvien.js
+++ bookstore/src/main/resources/static/user/js/thanh_nhanvien.js
@@ -83,27 +83,3 @@ chatInput.addEventListener("keydown", (e) => {
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
-var box  = document.getElementById('box');
-var down = false;
-
-
-function toggleNotifi(){
-	if (down) {
-		box.style.height  = '0px';
-		box.style.opacity = 0;
-		down = false;
-	}else {
-		box.style.height  = '510px';
-		box.style.opacity = 1;
-		down = true;
-	}
-}
-$(document).ready(function(){
-    $('.content').click(function(){
-      $('.content').toggleClass("heart-active")
-      $('.text').toggleClass("heart-active")
-      $('.numb').toggleClass("heart-active")
-      $('.heart').toggleClass("heart-active")
-    });
-  });
-
