d671482f12ca7a41d022b99f3f990a685cc9c4e1
thanhph25817 <thanhnvph25817@fpt.edu.vn>
2023-10-12 17:22:55 +0700

    thanh_CTSP

--- bookstore/src/main/resources/static/user/js/thanh_ctsp.js
+++ bookstore/src/main/resources/static/user/js/thanh_ctsp.js
@@ -106,4 +106,22 @@ $(document).ready(function(){
       $('.heart').toggleClass("heart-active")
     });
   });
+  var tabs = document.getElementsByClassName("notification-content");
+  function showTab(tabId) {
+      for (var i = 0; i < tabs.length; i++) {
+          tabs[i].style.display = "none";
+      }
+  
+      var selectedTab = document.getElementById(tabId);
+      if (selectedTab) {
+          selectedTab.style.display = "block";
+      }
+  }
+  showTab("tat-ca");
 
+  function myFunction(smallImg){
+    {
+        var fullImg=document.getElementById("imageBox")
+        fullImg.src=smallImg.src
+    }
+  }
