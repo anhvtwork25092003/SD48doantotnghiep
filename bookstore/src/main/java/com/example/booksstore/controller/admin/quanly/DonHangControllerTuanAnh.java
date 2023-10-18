1a65d45a47f7b1dd212c6abd9a3db6caee482a7a
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-10-18 21:20:54 +0700

    no message

--- bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/DonHangControllerTuanAnh.java
+++ bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/DonHangControllerTuanAnh.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/don-hang")
+@RequestMapping("/quan-ly")
 public class DonHangControllerTuanAnh {
-    @GetMapping("/cho-xac-nhan")
+    @GetMapping("/don-hang/cho-xac-nhan")
     public String hienThiTrangTongQuanQuanLy() {
         return "/admin/quanly/DonHangCho";
     }
 
-    @GetMapping("/cho-van-chuyen")
+    @GetMapping("/don-hang/cho-van-chuyen")
     public String quanLyChoVanChuyen() {
         return "/admin/quanly/DonHangDaDuyet";
     }
