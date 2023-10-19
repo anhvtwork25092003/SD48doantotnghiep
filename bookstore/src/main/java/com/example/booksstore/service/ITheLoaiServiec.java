dcc91a03deb9c58eb3cead21dcbd4193127b1aa4
sondt <sondtph23058@fpt.edu.vn>
2023-10-19 23:44:04 +0700

    nốt

--- bookstore/src/main/java/com/example/booksstore/service/ITheLoaiServiec.java
+++ bookstore/src/main/java/com/example/booksstore/service/ITheLoaiServiec.java
@@ -1,2 +1,14 @@
-package com.example.booksstore.service;public interface ITheLoaiServiec {
+package com.example.booksstore.service;
+
+import com.example.booksstore.entities.TheLoai;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface ITheLoaiServiec {
+    TheLoai voitheloai(TheLoai theLoai);
+
+    List<TheLoai> fillAll();
+
 }
--- bookstore/src/main/java/com/example/booksstore/service/serviceimpl/TheLoaiServiceImpl.java
+++ bookstore/src/main/java/com/example/booksstore/service/serviceimpl/TheLoaiServiceImpl.java
@@ -1,2 +1,28 @@
-package com.example.booksstore.service.serviceimpl;public class TheLoaiServiceImpl {
+package com.example.booksstore.service.serviceimpl;
+
+import com.example.booksstore.entities.TheLoai;
+import com.example.booksstore.repository.TheLoaiRepository;
+import com.example.booksstore.service.ITheLoaiServiec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TheLoaiServiceImpl implements ITheLoaiServiec {
+    @Autowired
+    private TheLoaiRepository theLoaiRepository;
+
+
+    @Override
+    public TheLoai voitheloai(TheLoai theLoai) {
+        return null;
+    }
+
+    @Override
+    public List<TheLoai> fillAll() {
+        return theLoaiRepository.findAll();
+    }
+
+
 }
