b2bcd740d349f734fc68f98bfbf12ea739051ac2
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-10-19 22:33:24 +0700

    up

--- bookstore/src/main/java/com/example/booksstore/service/ISachService.java
+++ bookstore/src/main/java/com/example/booksstore/service/ISachService.java
@@ -1,4 +1,9 @@
 package com.example.booksstore.service;
 
+import com.example.booksstore.entities.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ISachService {
+    Page<Sach> pageOfSach(Pageable pageable);
 }
--- bookstore/src/main/java/com/example/booksstore/service/serviceimpl/SachServiceImpl.java
+++ bookstore/src/main/java/com/example/booksstore/service/serviceimpl/SachServiceImpl.java
@@ -1,6 +1,20 @@
 package com.example.booksstore.service.serviceimpl;
 
+import com.example.booksstore.entities.Sach;
+import com.example.booksstore.repository.ISachRepository;
 import com.example.booksstore.service.ISachService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SachServiceImpl implements ISachService {
+
+    @Autowired
+    ISachRepository iSachRepository;
+    @Override
+    public Page<Sach> pageOfSach(Pageable pageable) {
+        return iSachRepository.findAll(pageable);
+    }
 }
