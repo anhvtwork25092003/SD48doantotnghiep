8bd40e235ea05ade04805022479220ab3ffa8128
sondt <sondtph23058@fpt.edu.vn>
2023-10-23 21:46:01 +0700

    ok

--- bookstore/src/main/java/com/example/booksstore/entities/TheLoai.java
+++ bookstore/src/main/java/com/example/booksstore/entities/TheLoai.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name="theloai")
+@ToString
 public class TheLoai {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +26,6 @@ public class TheLoai {
     private String moTa;
     @Column(name = "trangthai")
     private Integer trangThai;
+
 
 }
