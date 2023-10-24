1b5828c6b2c57580a38173c4ecd79f577d46fc41
thanhph25817 <thanhnvph25817@fpt.edu.vn>
2023-10-24 21:22:42 +0700

    thanh_tac

--- bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/TacGiaController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/TacGiaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,6 +22,33 @@ public class TacGiaController {
         model.addAttribute("data",t);
         return "admin/quanly/tacgia/thanh_tacgia";
     }
+
+    @GetMapping("/tac-gia/getcreate")
+    private  String getTacGiaCreate(Model model){
+        model.addAttribute("tacgia",new TacGia());
+        return "admin/quanly/tacgia/thanh_add_tacgia";
+    }
+
+    @PostMapping("/tac-gia/add")
+    private  String CreateTacgia(TacGia tacgia){
+        ser.createTacGia(tacgia);
+        return "redirect:/tac-gia/getall";
+    }
+
+    @GetMapping("/tac-gia/getct/{ma}")
+    private String getCt(@PathVariable("ma") Integer ma, TacGia tacGia, Model model){
+        TacGia t=ser.GetTacGiaByID(ma);
+        model.addAttribute("tacgia",t);
+        return "";
+    }
+    @PostMapping("/tac-gia/edit/{ma}")
+    public String Edit(@PathVariable("ma")Integer ma,TacGia tacGia,Model model){
+        TacGia t=ser.UpdateTacGia(ma,tacGia);
+        model.addAttribute("tacgia",t);
+        return "";
+    }
+
+
 
 
 }
