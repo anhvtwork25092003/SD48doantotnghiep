0d4869dca7f69184f0f280b52b37a3fc70be9476
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-11-04 11:55:15 +0700

    no message

--- bookstore/src/main/java/com/example/booksstore/service/serviceimpl/IKhuyenMaiService.java
+++
@@ -1,9 +0,0 @@
-package com.example.booksstore.service.serviceimpl;
-
-import com.example.booksstore.entities.KhuyenMai;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface IKhuyenMaiService {
-    abstract Page<KhuyenMai> getAllKhuyenMaiTheoTrangThai(Pageable pageable, int trangThai);
-}
