3b931e6dd0b4b3e8062fbcb8a25691043bddab4a
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-11-16 00:06:34 +0700

    sửa chữa db

---
+++ db/suadbngay1611.sql
@@ -0,0 +1,28 @@
+﻿select* from khachhang
+alter table diachi
+add idTinh int
+
+alter table diachi
+add idHuyen int
+
+alter table diachi
+add idXa int
+
+
+-- thêm idkhachhang vao bảng giỏ hàng
+alter table giohang 
+add idKhachHang int
+
+-- xóa cột idgiohang ở bảng khachhnag
+ALTER TABLE khachhang
+DROP CONSTRAINT FK_KhachHang_GioHang;
+ALTER TABLE KhachHang
+DROP CONSTRAINT UQ__KhachHan__CCE77A1EFF8D8F34;
+alter table khachhang
+drop column idGiohang
+-- thêm lại mối quân hệ giữa giỏ hàng và khách hàng thông quá khóa ngoại 
+-- Tạo khóa ngoại
+ALTER TABLE GioHang
+ADD CONSTRAINT FK_GioHang_KhachHang
+FOREIGN KEY (idKhachhang)
+REFERENCES KhachHang(idKhachhang);
