bf1b1e767a33d02aaf15f0484ef0505e23a629f8
phuocvvph23075 <phuocvvph23075@fpt.edu.vn>
2023-11-19 00:00:59 +0700

    push thong tin khach hang

--- bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/ThongtintaikhoanController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/ThongtintaikhoanController.java
@@ -25,38 +25,60 @@ public class ThongtintaikhoanController {
     IKhachHangRepository repository;
 
     @GetMapping("/thong-tin/hien-thi")
-    public String hienThiTrangQuanLyKhachHang(Model model, HttpSession session) {
-        session.setAttribute("loggedInUser", repository.findAll());
-        return "user/thongtintaikhoan";
+    public String hienThiThongTin(Model model, HttpSession session) {
+        // Retrieve the logged-in user from the session
+        KhachHang loggedInUser = (KhachHang) session.getAttribute("loggedInUser");
+
+        // Check if the user is logged in
+        if (loggedInUser != null) {
+            // Pass the user information to the Thymeleaf template
+            model.addAttribute("loggedInUser", loggedInUser);
+            return "user/thongtintaikhoan";
+        } else {
+            // Redirect to the login page if the user is not logged in
+            return "redirect:/login/khach-hang";
+        }
     }
 
-    @PostMapping("/thong-tin/cap-nhat")
-        public String thongtinsua(
-                @RequestParam("idKhachHang") String idKhachHang,
-                @RequestParam("hoVaTen") String hoVaTen,
-                @RequestParam("sdt") String sdt,
-                @RequestParam("ngaySinh") Date ngaySinh,
-                @RequestParam("gioiTinh") String gioiTinh,
-                @RequestParam("email") String email,
-                @RequestParam("trangThai") String trangThai,
-                @RequestParam("matKhau") String matKhau,
-                @RequestParam("loaiKhachHang") String loaiKhachHang,
-                @RequestParam("ngayTaoTaiKhoan") Date ngayTaoTaiKhoan
+//    @PostMapping("/thong-tin/cap-nhat")
+//        public String thongtinsua(
+//                @RequestParam("idKhachHang") String idKhachHang,
+//                @RequestParam("hoVaTen") String hoVaTen,
+//                @RequestParam("sdt") String sdt,
+//                @RequestParam("ngaySinh") Date ngaySinh,
+//                @RequestParam("gioiTinh") String gioiTinh,
+//                @RequestParam("email") String email,
+//                @RequestParam("trangThai") String trangThai,
+//                @RequestParam("matKhau") String matKhau,
+//                @RequestParam("loaiKhachHang") String loaiKhachHang,
+//                @RequestParam("ngayTaoTaiKhoan") Date ngayTaoTaiKhoan
+//
+//    ){
+//        KhachHang khachupdate= KhachHang.builder()
+//                .idKhachHang(Integer.parseInt(idKhachHang))
+//                .trangThai(Integer.parseInt(trangThai))
+//                .ngayTaoTaiKhoan(ngayTaoTaiKhoan)
+//                .matKhau(matKhau)
+//                .loaiKhachHang(loaiKhachHang)
+//                .hoVaTen(hoVaTen)
+//                .sdt(sdt)
+//                .ngaySinh(ngaySinh)
+//                .gioiTinh(Integer.parseInt(gioiTinh))
+//                .email(email)
+//                .build();
+//        this.khachHangService.save(khachupdate);
+//        return "redirect:/quan-ly/thong-tin/hien-thi";
+//    }
 
-    ){
-        KhachHang khachupdate= KhachHang.builder()
-                .idKhachHang(Integer.parseInt(idKhachHang))
-                .trangThai(Integer.parseInt(trangThai))
-                .ngayTaoTaiKhoan(ngayTaoTaiKhoan)
-                .matKhau(matKhau)
-                .loaiKhachHang(loaiKhachHang)
-                .hoVaTen(hoVaTen)
-                .sdt(sdt)
-                .ngaySinh(ngaySinh)
-                .gioiTinh(Integer.parseInt(gioiTinh))
-                .email(email)
-                .build();
-        this.khachHangService.save(khachupdate);
+    @PostMapping("/thong-tin/cap-nhat")
+    public String capNhatThongTin(@ModelAttribute("loggedInUser") KhachHang updatedUser, HttpSession session) {
+        // Update the user information in the database or service
+        // This is a simplified example; you would typically call a service method here
+        // to update the user information in the database
+         khachHangService.save(updatedUser);
+        // Update the user information in the session
+        session.setAttribute("loggedInUser", updatedUser);
+
         return "redirect:/quan-ly/thong-tin/hien-thi";
     }
 
