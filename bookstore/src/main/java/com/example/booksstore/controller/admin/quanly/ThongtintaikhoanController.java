cc3f0a92a553b4fdff6211e7b5a16e92da7eb2d3
phuocvvph23075 <phuocvvph23075@fpt.edu.vn>
2023-11-18 16:30:38 +0700

    huhu push nhầm

--- bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/ThongtintaikhoanController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/admin/quanly/ThongtintaikhoanController.java
@@ -1,15 +1,15 @@
 package com.example.booksstore.controller.admin.quanly;
 
 import com.example.booksstore.entities.DiaChi;
+import com.example.booksstore.entities.Sach;
+import com.example.booksstore.repository.IKhachHangRepository;
 import com.example.booksstore.service.IKhachHangService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import com.example.booksstore.entities.KhachHang;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -21,13 +21,15 @@ public class ThongtintaikhoanController {
     @Autowired
     IKhachHangService khachHangService;
 
-    List<KhachHang> pageOfKhachHang = new ArrayList<>();
+    @Autowired
+    IKhachHangRepository repository;
+
     @GetMapping("/thong-tin/hien-thi")
-    public String hienThiTrangQuanLyKhachHang(Model model) {
-        pageOfKhachHang=khachHangService.findAll();
-        model.addAttribute("pageOfKhachHang", pageOfKhachHang);
+    public String hienThiTrangQuanLyKhachHang(Model model, HttpSession session) {
+        session.setAttribute("loggedInUser", repository.findAll());
         return "user/thongtintaikhoan";
     }
+
     @PostMapping("/thong-tin/cap-nhat")
         public String thongtinsua(
                 @RequestParam("idKhachHang") String idKhachHang,
@@ -56,6 +58,14 @@ public class ThongtintaikhoanController {
                 .build();
         this.khachHangService.save(khachupdate);
         return "redirect:/quan-ly/thong-tin/hien-thi";
+    }
+
+    @GetMapping("/thong-tin/hien-thi/{idKhachHang}")
+    public String hienThiThongTin(@PathVariable Integer idKhachHang, HttpSession session) {
+            KhachHang khachHang = repository.getById(idKhachHang);
+            session.setAttribute("loggedInUser", khachHang);
+        return "user/thongtintaikhoan";
+//        return "redirect:/quan-ly/thong-tin/hien-thi";
     }
 
 }
