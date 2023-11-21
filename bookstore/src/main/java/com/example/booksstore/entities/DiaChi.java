1407606b088bd58dc6d35b1e77601d33ef5d0410
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-11-21 17:17:00 +0700

    no message

--- bookstore/src/main/java/com/example/booksstore/entities/DiaChi.java
+++ bookstore/src/main/java/com/example/booksstore/entities/DiaChi.java
@@ -1,5 +1,13 @@
 package com.example.booksstore.entities;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,6 +38,10 @@ public class DiaChi {
 
     @Column(name = "diachicuthe")
     private String diaChiCuThe;
+    @Column(name = "tennguoinhan")
+    private String tenNguoiNhan;
+    @Column(name = "sdtnguoinhanhang")
+    private String sdtNguoiNhanHang;
 
     @ManyToOne
     @JoinColumn(name = "idkhachhang")
