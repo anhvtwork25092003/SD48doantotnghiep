9a755bb64ed2f0b0014535e61f0e342defa32bf7
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-11-22 16:47:43 +0700

    no message

--- bookstore/src/main/java/com/example/booksstore/controller/user/ThanhToanController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/user/ThanhToanController.java
@@ -2,6 +2,7 @@ package com.example.booksstore.controller.user;
 
 import com.example.booksstore.entities.GioHangChiTiet;
 import com.example.booksstore.entities.KhachHang;
+import com.example.booksstore.entities.KhuyenMai;
 import com.example.booksstore.repository.IKhachHangRepository;
 import com.example.booksstore.repository.PhuongThucThanhToanRepo;
 import jakarta.servlet.http.HttpSession;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @RequestMapping("/thanh-toan")
@@ -75,6 +78,65 @@ public class ThanhToanController {
 
         } else {
             // đã đăng nhập rồi nè
+            // kiểm tra phương thức  thanh taons nè
+            if (Integer.parseInt(phuongThucThanhToan) == 1) {
+                // thanh toán với vnpay
+                // xử lý sau
+
+            } else {
+                // thanh toán tienf mặt khi nhận  hàng
+                // => tạo đơn nào
+                // đầu tiên lấy ra ngày tạo đơn
+                Date currentDate = new Date();
+                // lấy ra tổng tiền hàng nè
+                // duyệt list gioHangChiTietListForPay( giohangchitiet) => lấy sách => lấy tiền khuyến mãi
+
+                // b1: lấy tổng tiền hàng gốc
+                BigDecimal tongTienHangGoc = BigDecimal.ZERO;
+                for (GioHangChiTiet gioHangChiTiet : gioHangChiTietListForPay) {
+                    BigDecimal giaBan = gioHangChiTiet.getSach().getGiaBan();
+                    int soLuong = gioHangChiTiet.getSoLuong();
+                    BigDecimal thanhTien = giaBan.multiply(BigDecimal.valueOf(soLuong));
+                    tongTienHangGoc = tongTienHangGoc.add(thanhTien);
+                }
+                BigDecimal tongTienHangKhuyenMai = BigDecimal.ZERO;
+
+                // b2: lấy tổng tiền khuyến mãi
+                for (GioHangChiTiet gioHangChiTiet : gioHangChiTietListForPay) {
+                    //mỗi giỏ hàng chi tiết đi cùng  1 sách => đi cùng nhiều khuyến mãi => duyệt list lấy ra số phần
+                    // trăm giảm được áp dụng => sau đó tiến hành giảm trừ
+                    for (KhuyenMai khuyenMai : gioHangChiTiet.getSach().getKhuyenMais()) {
+                        if (khuyenMai.getTrangThai() == 1) {
+                            // khuyến mãi đang được áp dụng!
+                            // lây sra số phần trăm giảm giá
+                            double sophangiam = (khuyenMai.getSoPhanTramGiamGia()) / 100;
+                            BigDecimal thanhTienKhuyenMai = gioHangChiTiet.getSach().getGiaBan().multiply(BigDecimal.valueOf(sophangiam));
+                            BigDecimal.ZERO.add(thanhTienKhuyenMai);
+                            break;
+                        } else {
+                            tongTienHangKhuyenMai = BigDecimal.ZERO;
+                        }
+                    }
+                }
+
+
+                // phí vận chuyển fixx cứng 50 000
+                BigDecimal phiVanChuyen = new BigDecimal(50000);
+
+
+                // tổng tiền cần thanh toán: tiền hàng gốc - trừ tiền khuyến amix + tiền vận chuyển 50 000
+                BigDecimal tongTienCanThanhToan = tongTienHangGoc.subtract(tongTienHangKhuyenMai).add(phiVanChuyen);
+
+                // trạng thái sẽ là 0 = > chờ xác nhận lên đơn nè
+                int trangThai = 0;// chua xác nhạna
+                // ghi chú đơn hàng của khách hàng viết vào !
+// ghi chú trống hihih
+                // ghi chú lý do hủy đơn hàng
+
+                // khách hàng  = > lấy từ  sesion cho nhanh
+
+
+            }
         }
         return "/user/pay";
     }
--- bookstore/src/main/java/com/example/booksstore/entities/DonHangChiTiet.java
+++ bookstore/src/main/java/com/example/booksstore/entities/DonHangChiTiet.java
@@ -1,20 +1,26 @@
 package com.example.booksstore.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.sql.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name="donhangchitiet")
+@Table(name = "donhangchitiet")
 public class DonHangChiTiet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,6 +41,10 @@ public class DonHangChiTiet {
 
     @Column(name = "thanhtien")
     private BigDecimal thanhTien;
+
+    @ManyToOne
+    @JoinColumn(name = "iddonhang")
+    private DonHang donHang;
 
     @ManyToOne
     @JoinColumn(name = "idsach")
