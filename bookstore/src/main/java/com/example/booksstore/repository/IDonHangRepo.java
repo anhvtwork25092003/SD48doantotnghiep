bd395fbddb04c461c1babc83f0ca32d42d428ea6
phuocvvph23075 <phuocvvph23075@fpt.edu.vn>
2023-11-29 20:23:32 +0700

    no message

--- bookstore/src/main/java/com/example/booksstore/repository/IDonHangRepo.java
+++ bookstore/src/main/java/com/example/booksstore/repository/IDonHangRepo.java
@@ -2,10 +2,15 @@ package com.example.booksstore.repository;
 
 import com.example.booksstore.entities.DonHang;
 import com.example.booksstore.entities.KhachHang;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface IDonHangRepo extends JpaRepository<DonHang, Integer> {
     List<DonHang> findAllBykhachHang(KhachHang khachHang);
+    Page<DonHang> findAllByTrangThaiOrderByIdDonHang(Pageable pageable, int trangThai);
+    Page<DonHang> findByMaDonHangContainingAndKhachHang_SdtContaining(Pageable pageable,String maDonHang, String sdt);
+    DonHang findByIdDonHang (int idDonHang);
 }
