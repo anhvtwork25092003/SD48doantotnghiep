2ad85143773e8214bbdfc808c339dd8707f0ecb9
phuocvvph23075 <phuocvvph23075@fpt.edu.vn>
2023-11-29 18:12:03 +0700

    menuNhanVien

--- bookstore/src/main/java/com/example/booksstore/controller/admin/nhanvien/NhanVienController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/admin/nhanvien/NhanVienController.java
@@ -1,12 +1,22 @@
 package com.example.booksstore.controller.admin.nhanvien;
 
+import com.example.booksstore.entities.NhanVien;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NhanVienController {
     @GetMapping("/tong-quan-nhan-vien")
-    public String nhanVienTongQuan() {
-        return "/admin/nhanvien/layoutchungnhanvien/menuNhanVien";
+    public String nhanVienTongQuan(Model model, HttpSession session) {
+        NhanVien nhanVien = (NhanVien) session.getAttribute("loggedInUser");
+        if (nhanVien != null) {
+            model.addAttribute("loggedInUser", nhanVien);
+//            return "/admin/nhanvien/layoutchungnhanvien/menuNhanVien";
+//            return "/admin/quanly/DonHangCho";
+                return "redirect:/quan-ly/don-hang/cho-xac-nhan";
+        }
+        return "redirect:/login";
     }
 }
