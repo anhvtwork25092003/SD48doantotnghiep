7fe9dd75bd5ef1a7a8351a579ffa8cdb1cfa3d4b
vutuananh25092003@gmail.com <vutuananh25092003@gmail.com>
2023-12-04 19:57:42 +0700

    múa đi Thế Anh

--- bookstore/src/main/java/com/example/booksstore/controller/user/ThongBaoController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/user/ThongBaoController.java
@@ -1,10 +1,11 @@
 package com.example.booksstore.controller.user;
 
+import com.example.booksstore.entities.KhachHang;
 import com.example.booksstore.entities.ThongBaoKhachHang;
 import com.example.booksstore.repository.IThongBaoKhachHangRepo;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,21 +19,27 @@ public class ThongBaoController {
     @Autowired
     private IThongBaoKhachHangRepo thongBaoKhachHangRepository;
 
-    @GetMapping("/danh-sach-thong-bao/{userId}")
-    public List<ThongBaoKhachHang> getThongBaoForUser(@PathVariable Long userId) {
+    @GetMapping("/danh-sach-thong-bao}")
+    public List<ThongBaoKhachHang> getThongBaoForUser(HttpSession session) {
         try {
-            List<ThongBaoKhachHang> thongBaoList =
-                    thongBaoKhachHangRepository.findByKhachHangIdKhachHang(Integer.parseInt(String.valueOf(userId)));
-            if (!thongBaoList.isEmpty()) {
-                return thongBaoList;
-            } else {
-                // Nếu danh sách rỗng, bạn có thể trả về null hoặc throw một ngoại lệ và xử lý nó ở một tầng khác.
+            KhachHang khachHangDangNhap = (KhachHang) session.getAttribute("loggedInUser");
+            if (khachHangDangNhap != null) {
+                List<ThongBaoKhachHang> thongBaoList =
+                        thongBaoKhachHangRepository.findByKhachHangIdKhachHang(khachHangDangNhap.getIdKhachHang());
+                if (!thongBaoList.isEmpty()) {
+                    return thongBaoList;
+                } else {
+                    // Nếu danh sách rỗng, bạn có thể trả về null hoặc throw một ngoại lệ và xử lý nó ở một tầng khác.
+                    return null;
+                }
+            }else{
                 return null;
             }
+
         } catch (Exception e) {
             // Xử lý lỗi và log
             return Collections.emptyList(); // Hoặc trả về danh sách trống tùy thuộc vào yêu cầu cụ thể của bạn
         }
     }
-    
+
 }
