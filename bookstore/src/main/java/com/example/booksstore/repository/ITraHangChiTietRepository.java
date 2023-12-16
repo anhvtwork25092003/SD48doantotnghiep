2b289b7fae2ee3459b00726985de9165640c0126
Phạm Thế Anh <theanhpham665@gmail.com>
2023-12-16 19:43:30 +0700

    Đổi hàng lên

--- bookstore/src/main/java/com/example/booksstore/repository/ITraHangChiTietRepository.java
+++ bookstore/src/main/java/com/example/booksstore/repository/ITraHangChiTietRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITraHangChiTietRepository extends JpaRepository<TraHangChiTiet, Integer> {
     Page<TraHangChiTiet> findAll(Pageable pageable);
+    List<TraHangChiTiet> findAllByTraHang_IdTraHang(Integer idTraHang);
 }
