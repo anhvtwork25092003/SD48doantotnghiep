d5449c89576d8669dca91275912403990c7aecd3
thanhph25817 <thanhnvph25817@fpt.edu.vn>
2023-12-18 17:05:09 +0700

    ThongTin

--- bookstore/src/main/java/com/example/booksstore/controller/user/ThongTinController.java
+++ bookstore/src/main/java/com/example/booksstore/controller/user/ThongTinController.java
@@ -1,2 +1,57 @@
-package com.example.booksstore.controller.user;public class ThongTinController {
+package com.example.booksstore.controller.user;
+
+
+import com.example.booksstore.entities.ThongTin;
+import com.example.booksstore.repository.IThongTinRepository;
+import com.example.booksstore.service.IThongTinService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+@RequestMapping("/quan-ly")
+public class ThongTinController {
+    @Autowired
+    IThongTinService service;
+    @Autowired
+    IThongTinRepository repository;
+
+    @GetMapping("/thong_tin/thong-tin/thanh")
+    public String GetThongTin(HttpSession session, Model model) {
+        ThongTin loggedInThongTin = (ThongTin) session.getAttribute("loggedInThongTin");
+        model.addAttribute("loggedInThongTin", loggedInThongTin);
+        return "user/thongtin/thanh_thongtin";
+    }
+
+    @PostMapping("/thong-tin/cap-nhat/moi")
+    public String thongtinthaydoimoi(
+            @RequestParam("idThongTin") Integer id,
+            @RequestParam("diaChi") String diaChi,
+            @RequestParam("soDienThoai1") String soDienThoai1,
+            @RequestParam("soDienThoai2") String soDienThoai2,
+            @RequestParam("email") String email,
+            @RequestParam("linkBannerTrangChu") MultipartFile linkAnhTrangChu,
+            @RequestParam(" linkLogo") MultipartFile linklogo
+    ) {
+        try {
+            ThongTin thongTin = service.getOne((id));
+            ThongTin thongTinupdate = ThongTin.builder()
+                    .idThongTin(thongTin.getIdThongTin())
+                    .email(email)
+                    .diaChi(diaChi)
+                    .soDienThoai1(soDienThoai1)
+                    .soDienThoai2(soDienThoai2).build();
+            service.AddThongTin(thongTinupdate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:user/thongtin/thanh_thongtin" + id;
+    }
 }
--- bookstore/src/main/java/com/example/booksstore/entities/ThongTin.java
+++ bookstore/src/main/java/com/example/booksstore/entities/ThongTin.java
@@ -1,10 +1,7 @@
 package com.example.booksstore.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Date;
 
@@ -13,6 +10,7 @@ import java.sql.Date;
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name="thongtin")
 public class ThongTin {
     @Id
